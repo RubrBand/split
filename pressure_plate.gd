41d49cfb36b208823106fd3a22c9e807c1a2a076
Iris <ingussm.is@gmail.com>
2021-10-02 19:48:28 +0300

    level logistics 101

---
+++ pressure_plate.gd
@@ -0,0 +1,28 @@
+extends "res://levelnode.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var memindex = 0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func update():
+	pass
+
+func onenter():
+	pass
+
+func onexit():
+	pass
+
+func undo():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
