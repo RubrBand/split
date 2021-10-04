cd2ab2c6bea2d680a9bb8778c3a80d001e752979
Iris <ingussm.is@gmail.com>
2021-10-05 00:55:40 +0300

    level 8

--- Scripts/or.gd
+++ Scripts/or.gd
@@ -7,11 +7,15 @@ extends "res://Scripts/levelnode.gd"
 export var memindex = 0
 export var var1 = 0
 export var var2 = 0
+export var type = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func update():
-	level.set_logic(memindex, min(level.get_logic(var1)+level.get_logic(var2),1))
+	if type == 0:
+		level.set_logic(memindex, min(level.get_logic(var1)+level.get_logic(var2),1))
+	elif type == 1:
+		level.set_logic(memindex, level.get_logic(var1)*level.get_logic(var2))
 
